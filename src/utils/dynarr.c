acdc8eb8c825b17f4cd7a2df967903a21e75749e
Justin Garza <jgarza9788@gmail.com>
2026-03-16 21:25:10 -0400

    utils: fix dynarr_join empty-array underflow

--- src/utils/dynarr.c
+++ src/utils/dynarr.c
@@ -3,18 +3,24 @@
 
 #include "dynarr.h"
 char *dynarr_join(char **arr, const char *sep) {
+	if (dynarr_is_empty(arr)) {
+		dynarr_free_pod(arr);
+		return strdup("");
+	}
+
 	size_t total_len = 0;
 	dynarr_foreach(arr, i) {
 		total_len += strlen(*i);
 	}
 
-	char *ret = malloc(total_len + strlen(sep) * (dynarr_len(arr) - 1) + 1);
+	size_t sep_len = strlen(sep);
+	char *ret = malloc(total_len + sep_len * (dynarr_len(arr) - 1) + 1);
 	size_t pos = 0;
 	allocchk(ret);
 	dynarr_foreach(arr, i) {
 		if (i != arr) {
 			strcpy(ret + pos, sep);
-			pos += strlen(sep);
+			pos += sep_len;
 		}
 		strcpy(ret + pos, *i);
 		pos += strlen(*i);
@@ -23,4 +29,12 @@ char *dynarr_join(char **arr, const char *sep) {
 	dynarr_free_pod(arr);
 	ret[pos] = '\0';
 	return ret;
+}
+
+
+TEST_CASE(dynarr_join_empty) {
+	char **arr = dynarr_new(char *, 0);
+	char *joined = dynarr_join(arr, ", ");
+	TEST_STREQUAL(joined, "");
+	free(joined);
 }
